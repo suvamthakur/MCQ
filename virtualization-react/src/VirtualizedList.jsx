b0e140114442acff9415817b66577a5ba74920aa
suvamthakur <suvamthakur2005@gmail.com>
2025-12-06 13:27:38 +0530

    optimization

--- virtualization-react/src/VirtualizedList.jsx
+++ virtualization-react/src/VirtualizedList.jsx
@@ -1,21 +1,47 @@
-import { useState } from "react";
+import { useCallback, useMemo, useState } from "react";
+
+function throttle(fn, delay) {
+  let timerId = null;
+  return function (...args) {
+    if (!timerId) {
+      timerId = setTimeout(() => {
+        timerId = null;
+        fn(...args);
+      }, delay);
+    }
+  };
+}
 
 export default function VirtualizedList({
   width = "100%",
   height = 600,
   rows = 20,
   items = [],
+  overscan = 5,
 }) {
   const ITEM_HEIGHT = height / rows;
   const [startIndex, setStartIndex] = useState(0);
 
-  const handleScroll = (e) => {
-    const scrollTop = e.target.scrollTop;
+  const handleScroll = useCallback(
+    (e) => {
+      console.log("call");
+      const scrollTop = e.target.scrollTop;
 
-    const newStartIndex = Math.floor(scrollTop / ITEM_HEIGHT);
-    setStartIndex(newStartIndex);
-  };
-  const finalItems = items.slice(startIndex, startIndex + rows + 1);
+      const newStartIndex = Math.floor(scrollTop / ITEM_HEIGHT);
+      setStartIndex(newStartIndex);
+    },
+    [ITEM_HEIGHT]
+  );
+
+  const throttleHandleScroll = useMemo(
+    () => throttle(handleScroll, 100),
+    [handleScroll]
+  );
+
+  const finalItems = useMemo(
+    () => items.slice(startIndex, startIndex + rows + overscan),
+    [items, startIndex, rows, overscan]
+  );
 
   return (
     <div>
@@ -27,25 +53,32 @@ export default function VirtualizedList({
           overflow: "auto",
           backgroundColor: "khaki",
         }}
-        onScroll={handleScroll}
+        onScroll={throttleHandleScroll}
       >
         <div
-          style={{ height: ITEM_HEIGHT * items.length, position: "relative" }}
+          style={{ height: items.length * ITEM_HEIGHT, position: "relative" }}
         >
-          {finalItems.map((val, index) => (
-            <div
-              key={val}
-              style={{
-                height: ITEM_HEIGHT - 2,
-                borderTop: "1px solid black",
-                position: "absolute",
-                width: "100%",
-                top: (startIndex + index) * ITEM_HEIGHT,
-              }}
-            >
-              {val}
-            </div>
-          ))}
+          <div
+            style={{
+              position: "absolute",
+              top: 0,
+              left: 0,
+              width: "100%",
+              transform: `translateY(${startIndex * ITEM_HEIGHT}px)`,
+            }}
+          >
+            {finalItems.map((item, i) => (
+              <div
+                key={startIndex + i}
+                style={{
+                  height: ITEM_HEIGHT,
+                  borderBottom: "1px solid black",
+                }}
+              >
+                {item}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     </div>
